0d2fa0fb86990d769ab29d91694f8f3b736b1edb
NabilShahid <nabil110176@gmail.com>
2019-07-08 16:18:29 +0500

    Main controlller code cleanup

--- src/components/MainScroller/mainscroller.jsx
+++ src/components/MainScroller/mainscroller.jsx
@@ -1,51 +1,54 @@
 import React, { Component } from "react";
 import "./mainscroller.css";
 import ScrollSection from "../ScrollSection/scrollsection";
-import {createParticles} from "../../services/particlesJS";
+import { createParticles } from "../../services/particlesJS";
 class MainScroller extends Component {
   state = {};
   componentDidMount() {
-	// window.VANTA.WAVES({
-	// 	el: "#maaCanvas"
-	//   })
-	createParticles()
+    createParticles();
   }
   render() {
     return (
-      
-      <div id="mainScrollerDiv" data-hijacking="on" data-animation="fixed">
-    
+      <div id="mainScrollerDiv" data-hijacking="on" data-animation="parallax">
         {/* <!-- hijacking: on/off - animation: none/scaleDown/rotate/gallery/catch/opacity/fixed/parallax --> */}
-		{/* <div id="maaCanvas" style={{width:"100vw",height:"100vh"}}></div> */}
-		
-		{/* <div id="particles-js" style={{width:"100vw",height:"100vh"}}></div> 
+        {/* <div id="maaCanvas" style={{width:"100vw",height:"100vh"}}></div> */}
+
+        {/* <div id="particles-js" style={{width:"100vw",height:"100vh"}}></div> 
 		 <div className="count-particles"> <span className="js-count-particles">--</span> particles </div> */}
         <ScrollSection
           visible={true}
-          background="rgb(26, 32, 64)"
-		  heading="Section 1"
-      bgAnimId="particles-js"
-      sectionId="section-1"
+          background="linear-gradient(to right bottom, #051037, #021446, #021755, #031964, #0a1b73)"
+          heading="Section 1"
+          bgAnimId="particles-js"
+          sectionId="section-1"
         />
-        <ScrollSection background="blue" heading="Section 2" sectionId="section-2"/>
-        <ScrollSection background="green" heading="Section 3" sectionId="section-3" />
-        <ScrollSection background="yellow" sectionId="section-4" heading="Section 4" />
+        <ScrollSection
+          background="blue"
+          heading="Section 2"
+          sectionId="section-2"
+        />
+        <ScrollSection
+          background="green"
+          heading="Section 3"
+          sectionId="section-3"
+        />
+        <ScrollSection
+          background="yellow"
+          sectionId="section-4"
+          heading="Section 4"
+        />
         <ScrollSection
           background="orange"
           heading="Section 5"
           sectionId="section-5"
         />
-          <nav>
+        <nav>
           <ul className="cd-vertical-nav">
             <li>
-              <a className="cd-prev inactive">
-                Next
-              </a>
+              <a className="cd-prev inactive">Next</a>
             </li>
             <li>
-              <a className="cd-next">
-                Prev
-              </a>
+              <a className="cd-next">Prev</a>
             </li>
           </ul>
         </nav>
@@ -57,7 +60,7 @@ class MainScroller extends Component {
               </a>
             </li>
             <li>
-              <a className="main-nav-item"  id="section-2-Link">
+              <a className="main-nav-item" id="section-2-Link">
                 Section 2
               </a>
             </li>
