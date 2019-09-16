02039a908acf4a227aaf1a19cc39ec939373ad28
NabilShahid <nabil110176@gmail.com>
2019-09-17 00:59:14 +0500

    Rotating text intro implementation

--- src/components/IntroText/introtext.jsx
+++ src/components/IntroText/introtext.jsx
@@ -1,21 +1,112 @@
-import React from 'react';
+import React from "react";
 import "./introtext.css";
+setTimeout(() => {
+  var words = document.getElementsByClassName("word");
+  var wordArray = [];
+  var currentWord = 0;
+
+  words[currentWord].style.opacity = 1;
+  for (var i = 0; i < words.length; i++) {
+    splitLetters(words[i]);
+  }
+
+  function changeWord() {
+    var cw = wordArray[currentWord];
+    var nw =
+      currentWord == words.length - 1
+        ? wordArray[0]
+        : wordArray[currentWord + 1];
+    for (var i = 0; i < cw.length; i++) {
+      animateLetterOut(cw, i);
+    }
+
+    for (var i = 0; i < nw.length; i++) {
+      nw[i].className = "letter behind";
+      nw[0].parentElement.style.opacity = 1;
+      animateLetterIn(nw, i);
+    }
+
+    currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+  }
+
+  function animateLetterOut(cw, i) {
+    setTimeout(function() {
+      cw[i].className = "letter out";
+    }, i * 80);
+  }
+
+  function animateLetterIn(nw, i) {
+    setTimeout(function() {
+      nw[i].className = "letter in";
+    }, 340 + i * 80);
+  }
+
+  function splitLetters(word) {
+    var content = word.innerHTML;
+    word.innerHTML = "";
+    var letters = [];
+    for (var i = 0; i < content.length; i++) {
+      var letter = document.createElement("span");
+      letter.className = "letter";
+      letter.innerHTML = content.charAt(i);
+      if(letter.innerHTML==" ")letter.innerHTML="&nbsp;"
+      console.log('content.charAt(i) :', content.charAt(i));
+      word.appendChild(letter);
+      letters.push(letter);
+    }
+
+    wordArray.push(letters);
+  }
+
+  changeWord();
+  setInterval(changeWord, 4000);
+});
 const IntroText = () => {
-    return (      <div class="animcontent">
-    <div class="content__container">
-      <p class="content__container__text">
-        Hello
-      </p>
-      
-      <ul class="content__container__list">
-        <li class="content__container__list__item">1</li>
-        <li class="content__container__list__item">2</li>
-        <li class="content__container__list__item">3</li>
-        <li class="content__container__list__item">4</li>
-        {/* <li class="content__container__list__item">5</li> */}
-      </ul>
-    </div>
-  </div> );
-}
- 
+  return (
+    
+       <div className="textDiv">
+       <p className="rotateTextP">Hi, I am a </p>
+       <p style={{marginLeft:"10px"}} className="rotateTextP">
+         <span className="word">FullStack Developer</span>
+         <span className="word">Front End Developer</span>
+         <span className="word">Backend Engineer</span>
+         <span className="word">JavaScript Developer</span>
+         <span className="word">Database Programmer</span>        
+       </p>
+     </div>
+     
+   
+
+    // <div>
+    //   <div className="Iam">
+    //     <p>This is</p>
+    //     <b>
+    //       <div className="innerIam">
+    //         leggera
+    //         <br />
+    //         a theme in progress
+    //         <br />
+    //         built on bootstrap
+    //         <br />
+    //         how I learn stuff
+    //         <br />
+    //         how we do it
+    //       </div>
+    //     </b>
+    //   </div>
+    // </div>
+
+    // <div className="textDiv">
+    //   <p className="rotateTextP">Hi, I am a </p>
+    //   <p style={{marginLeft:"10px"}} className="rotateTextP">
+    //     <span className="word">FullStack Developer</span>
+    //     <span className="word">Front End Developer</span>
+    //     <span className="word">Backend Engineer</span>
+    //     <span className="word">JavaScript Developer</span>
+    //     <span className="word">Database Programmer</span>        
+    //   </p>
+    // </div>
+  );
+};
+
 export default IntroText;
