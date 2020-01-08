236237ed814edf61ed5a867ccc8d971ef4a9f87c
NabilShahid <nabil110176@gmail.com>
2020-01-08 22:35:26 +0500

    User visit record

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import "animate.css/animate.min.css";
 import MainScroller from './components/MainScroller/mainscroller'
+import {reacordUserVisit} from "./services/api";
 ReactDOM.render(<MainScroller />, document.getElementById('root'));
+reacordUserVisit();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
